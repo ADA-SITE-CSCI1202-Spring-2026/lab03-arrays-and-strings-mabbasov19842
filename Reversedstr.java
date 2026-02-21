17b0785a746c5d473c403072ac89e04a9724cd1c
mabbasov19842 <167711233+mabbasov19842@users.noreply.github.com>
2026-02-21 11:11:33 +0400

    Add files via upload

---
+++ Reversedstr.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Reversedstr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    System.out.print("enter a string: ");
+        String input = sc.nextLine();
+
+    String reversed = "";
+
+    for (int i = input.length() - 1; i >= 0; i--) {
+            reversed = reversed + input.charAt(i);
+        }
+        
+        System.out.println("Reversed string: " + reversed);
+        
+        sc.close();
+}   
+}
